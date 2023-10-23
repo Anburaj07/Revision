0a2454e54fe90cae2b929fa37b6917c11c7f1f2e
Anburaj S <anburaj2203@gmail.com>
2023-10-23 19:11:51 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Button, Flex, Heading, Spacer } from "@chakra-ui/react";
 import { MoonIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
 const Navbar = () => {
+    const [theme,setTheme]=useState(true)
   return (
     <Flex
       minWidth="max-content"
       p="2"
       align="center"
       justify="space-around"
-      color="white"
+      color={theme==true?"white":"black"}
     >
       <Link to="/">
         <Box p="2">
           <Heading size="md">TODO</Heading>
         </Box>
       </Link>
-      <MoonIcon boxSize={6} />
+      <MoonIcon boxSize={6} onClick={(e)=>{setTheme(!theme)}} />
     </Flex>
   );
 };
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -5,44 +5,84 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 
 export const Todos = () => {
   let data = useSelector((store) => store.reducer.todos);
-  const [todos,setTodos]=useState(data)
-  console.log(data,'in homr');
+  const [todos, setTodos] = useState(data);
+  console.log(data, "in homr");
 
   // useEffect(()=>{
   //   handleActive()
   // },[data])
 
-  const handleAll=()=>{
-    setTodos(data)
-  }
-  const handleActive=()=>{
-    let activeData=data.filter((el)=>{
-      if(el.active){
-        return el
+  const handleAll = () => {
+    setTodos(data);
+  };
+  const handleActive = () => {
+    let activeData = data.filter((el) => {
+      if (el.active) {
+        return el;
       }
-    })
-    setTodos(activeData)
-  }
+    });
+    setTodos(activeData);
+  };
 
-  const handleCompleted=()=>{
-    let activeData=data.filter((el)=>{
-      if(el.status){
-        return el
+  const handleCompleted = () => {
+    let activeData = data.filter((el) => {
+      if (el.status) {
+        return el;
       }
-    })
-    setTodos(activeData)
-  }
+    });
+    setTodos(activeData);
+  };
+  const handleClearCompleted = () => {};
   return (
-    <Box mt="5" bgColor="whitesmoke" color="black"  w="70%" >
+    <Box mt="5" bgColor="whitesmoke" color="black" w="70%">
       {todos.map((el) => (
         <TodoCard {...el} />
       ))}
-      <Flex p='4' justify='space-around'>
-        <Text color='blackAlpha.600'>{todos.length}itesm left</Text>
-        <Text color='blackAlpha.600' onClick={handleAll}>All</Text>
-        <Text color='blackAlpha.600' onClick={handleActive}>Active</Text>
-        <Text color='blackAlpha.600' onClick={handleCompleted}>Completed</Text>
-        <Text color='blackAlpha.600'>Clear Completed</Text>
+      <Flex p="4" justify="space-around">
+        <Text
+          color="blackAlpha.600"
+          _hover={{
+            fontWeight: "semibold",
+          }}
+        >
+          {todos.length}itesm left
+        </Text>
+        <Text
+          color="blackAlpha.600"
+          _hover={{
+            fontWeight: "semibold",
+          }}
+          onClick={handleAll}
+        >
+          All
+        </Text>
+        <Text
+          color="blackAlpha.600"
+          _hover={{
+            fontWeight: "semibold",
+          }}
+          onClick={handleActive}
+        >
+          Active
+        </Text>
+        <Text
+          color="blackAlpha.600"
+          _hover={{
+            fontWeight: "semibold",
+          }}
+          onClick={handleCompleted}
+        >
+          Completed
+        </Text>
+        <Text
+          color="blackAlpha.600"
+          _hover={{
+            fontWeight: "semibold",
+          }}
+          onClick={handleClearCompleted}
+        >
+          Clear Completed
+        </Text>
       </Flex>
     </Box>
   );
